24826e83c343fa2b2cfe0e96403bc5ae924a9fde
Matías M <matiasmscl@gmail.com>
2023-11-25 09:09:33 -0300

    feat: Peliculas

--- Add_pelicula.py
+++ Add_pelicula.py
@@ -12,18 +12,36 @@ try:
     driver.find_element(By.ID, ":r1:-tab-0").click()
 
     add_movie_button = driver.find_element(By.CSS_SELECTOR, "#add-movie-btn > .flex")
+
     ActionChains(driver).move_to_element(add_movie_button).click().perform()
     ActionChains(driver).move_to_element(add_movie_button).release().perform()
 
-    driver.find_element(By.ID, "title").send_keys("Pelicula6")
-    driver.find_element(By.ID, "description").send_keys("Descripcion6")
-    driver.find_element(By.ID, "year").send_keys("19955")
-
+    driver.find_element(By.ID, "title").send_keys("Pelicula 1")
+    driver.find_element(By.ID, "description").send_keys("Descripcion 1")
+    driver.find_element(By.ID, "year").send_keys("2001")
     director_dropdown = driver.find_element(By.ID, "director_id")
-    Select(director_dropdown).select_by_visible_text("Director 6")
+    Select(director_dropdown).select_by_visible_text("Director 1")
 
     add_movie_button = driver.find_element(By.CSS_SELECTOR, ".h-min > .flex")
     ActionChains(driver).move_to_element(add_movie_button).click().perform()
+    ActionChains(driver).move_to_element(add_movie_button).release().perform()
+
+
+    add_movie_button = driver.find_element(By.CSS_SELECTOR, "#add-movie-btn > .flex")
+
+    ActionChains(driver).move_to_element(add_movie_button).click().perform()
+    ActionChains(driver).move_to_element(add_movie_button).release().perform()
+
+    driver.find_element(By.ID, "title").send_keys("Pelicula 2")
+    driver.find_element(By.ID, "description").send_keys("Descripcion 2")
+    driver.find_element(By.ID, "year").send_keys("2002")
+    director_dropdown = driver.find_element(By.ID, "director_id")
+    Select(director_dropdown).select_by_visible_text("Director 2")
+
+    add_movie_button = driver.find_element(By.CSS_SELECTOR, ".h-min > .flex")
+    ActionChains(driver).move_to_element(add_movie_button).click().perform()
+    ActionChains(driver).move_to_element(add_movie_button).release().perform()
+
 
 except Exception as e:
     print(f"Error: {str(e)}")
